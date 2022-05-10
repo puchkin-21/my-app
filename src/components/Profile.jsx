6618140c8c1117af10eb0026610f19380d251d36
puchkin-21 <mpuchkin@list.ru>
2022-05-10 19:33:14 +0300

    first commit

---
+++ src/components/Profile.jsx
@@ -0,0 +1,24 @@
+const Profile = () => {
+  return <div className='content'>
+    <div>
+      <img src='https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg' />
+    </div>
+    <div>
+      av+desc
+    </div>
+    <div>
+      My posts
+      <div>
+        New posts
+      </div>
+      <div>
+        Post 1
+      </div>
+      <div>
+        Post 2
+      </div>
+    </div>
+  </div>
+}
+
+export default Profile
